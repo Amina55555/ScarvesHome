ee92391b7b71f6701866da7508d02a926ca340c8
Amina55555 <57752741+Amina55555@users.noreply.github.com>
2020-02-19 16:35:04 +0100

    enlever le s de page

--- app/Controller/HomePageController.php
+++ app/Controller/HomePageController.php
@@ -8,38 +8,39 @@ use Carbon\Carbon;
 // Liste des articles
 
 
-class HomePageController{
-
-public static function listArticles()
+class HomePageController
 {
-	$articleManager = new ArticleManager();
-    $articles = $articleManager->readAll();
 
-    require('view/homepage/index.php');
-}
+    public static function listArticles()
+    {
+        $articleManager = new ArticleManager();
+        $articles = $articleManager->readAll();
 
-public static function getArticleById($idArticle) 
-{
-	//$articleManager = new \Blogz\Model\ArticleManager();
-	$articleManager = new ArticleManager();
-    $article = $articleManager->readArticle($idArticle);
+        require('view/homepage/index.php');
+    }
 
-    //ICI le controle et création des variables a envoyer à la vue
-    if( $article->rowCount() == 1 ){
-		// on va chercher le data
-		$data = $article->fetch();
-		
-		// on écrase nos variables à afficher dasn le form
-		$nom 			= $data['article_nom'];
-		$contributeur 	= $data['article_contributeur'];
-		$texte 			= $data['article_texte'];
-		$date 			= $data['article_date'];
-		
-		// on modifie le titre
-		$titre = 'Modification';
-		$today = Carbon::now()->isoFormat('LLLL');
-	}    
+    public static function getArticleById($idArticle)
+    {
+        //$articleManager = new \Blogz\Model\ArticleManager();
+        $articleManager = new ArticleManager();
+        $article = $articleManager->readArticle($idArticle);
 
-    require('view/formpage/index.php');
-}
+        //ICI le controle et création des variables a envoyer à la vue
+        if ($article->rowCount() == 1) {
+            // on va chercher le data
+            $data = $article->fetch();
+
+            // on écrase nos variables à afficher dasn le form
+            $nom = $data['article_nom'];
+            $contributeur = $data['article_contributeur'];
+            $texte = $data['article_texte'];
+            $date = $data['article_date'];
+
+            // on modifie le titre
+            $titre = 'Modification';
+            $today = Carbon::now()->isoFormat('LLLL');
+        }
+
+        require('view/formpage/index.php');
+    }
 }
